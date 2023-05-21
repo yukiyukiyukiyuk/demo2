a6a99b86afd04badd3c302862760d723ee9ac237
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 17:36:02 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -35,5 +35,7 @@ urlpatterns = [
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
