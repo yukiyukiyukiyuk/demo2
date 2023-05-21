459605689ae8cb22868b5227a6ddc305e5579d34
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 18:04:37 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,6 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORONGE = "whitenoise.storage.CompressedManifestStaticFileStorage"
 
 # settings.py
 SITE_TITLE = "Infection AI"
