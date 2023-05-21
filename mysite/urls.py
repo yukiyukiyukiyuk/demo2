c0632e445466d40ec833c0bfce4ee158a36b4c6d
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 16:36:54 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,10 +33,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("flower/", include("flower.urls")),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-]
-urlpatterns = [
-  url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-  url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
