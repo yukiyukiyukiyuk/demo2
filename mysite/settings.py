dcf4a7aa843c311e623f37c5076d34e547c2dea8
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 18:08:31 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_STORONGE = "whitenoise.storage.CompressedManifestStaticFileStorage"
 
 # settings.py
