70cf6836dfd1174b546e483f1960961f1996e11f
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 17:29:00 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
@@ -33,7 +33,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("flower/", include("flower.urls")),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
