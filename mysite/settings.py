24fa2e7fb2f7b55700f59a4013fdf551dc08753f
yukiyukiyukiyuk <101117328+yukiyukiyukiyuk@users.noreply.github.com>
2023-05-21 21:39:52 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,7 +118,7 @@ if DEBUG:
 
 else:
 
-  STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+  STATIC_ROOT =  "/home/ec2-user/demo/static"
   
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
